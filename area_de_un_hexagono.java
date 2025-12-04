833286774760836d3c3950adbcad9b0f572c91ab
Angel Tadeo Pascual Tapia <250110521@itsoeh.edu.mx>
2025-12-04 11:02:37 -0600

    si

---
+++ area_de_un_hexagono.java
@@ -0,0 +1,21 @@
+    import java.util.Scanner;
+public class area_de_un_hexagono {
+    public static void main (String[]args){
+        //DATOS DEL CODIGO
+      double  area, apotema, perimetro ;
+        
+        Scanner val  = new Scanner(System.in);
+
+        System.out.print("Ingrese el valor de hexagono: ");
+        apotema = val.nextFloat();
+        
+        System.out.print("Ingrese el perimetro del hexagono : ");
+        perimetro = val.nextFloat();
+
+        area = (apotema * perimetro)/2;
+       
+
+        System.out.println("Área del Hexagono: " + area);   
+    }    
+}
+
